e830b950de700e3b64c912130a005d2624d76aef
leoklems <leoklems@yahoo.com>
2023-01-23 04:03:47 +0100

    adding the previous works section

--- assets/js/main.js
+++ assets/js/main.js
@@ -87,5 +87,58 @@ $(document).ready(function() {
         $('.menu_wrd').toggleClass('menu_wrd_trigg');
 
     })
+    currentSlide();
 
 });
+
+let slideIndex = 0;
+
+function plusSlides(n) {
+
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    if (n >= slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    slideIndex += n;
+    console.log(slideIndex)
+    slideIndex++;
+
+
+
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    slides[slideIndex - 1].style.display = "grid";
+    // showSlides();
+
+}
+
+function currentSlide() {
+    showSlides();
+}
+
+function showSlides() {
+    // console.log(slideIndex)
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+
+
+
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    slides[slideIndex - 1].style.display = "grid";
+    setTimeout(showSlides, 7000);
+
+}
